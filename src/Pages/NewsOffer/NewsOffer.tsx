8430abda7e315c0277d6866dc02a1f9ace77eb15
= <=>
2023-05-30 00:19:14 +0700

    update

---
+++ src/Pages/NewsOffer/NewsOffer.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Layout } from "../../components/Layout";
+import { Tabs } from "../../components/Tabs";
+import { useGet } from "../../api/get";
+import { NewsOffer as NewsOfferInterface } from "../../interface/Interface";
+import { useNavigate } from "react-router-dom";
+import { CalendarDaysIcon } from "@heroicons/react/24/outline";
+import { Spin } from "antd";
+import errImage from "../../assets/img/news_img_n1.jpg";
+
+export const NewsOffer = () => {
+    const navigate = useNavigate();
+    const [selectedTab, setSelectedTab] = useState<boolean>(true);
+
+    const {
+        fetchGet,
+        result: newsOffers,
+        isLoading,
+    } = useGet<NewsOfferInterface[]>();
+
+    useEffect(() => {
+        fetchGet("newsoffer/offer");
+    }, []);
+
+    useEffect(() => {
+        if (selectedTab) fetchGet("newsoffer/offer");
+        else fetchGet("newsoffer/news");
+    }, [selectedTab]);
+
+    return (
+        <>
+            <div className="bg-white dark:bg-transparent dark:text-white">
+                <Tabs
+                    selectedTab={selectedTab}
+                    setSelectedTab={setSelectedTab}
+                    tab1="ƯU ĐÃI"
+                    tab2="TIN MỚI"
+                ></Tabs>
+
+                {isLoading ? (
+                    <div className="flex justify-center min-h-screen">
+                        <Spin size="large" tip="Loading..." />
+                    </div>
+                ) : (
+                    <div className="flex flex-wrap justify-center lg:gap-y-10 gap-y-5 lg:gap-x-10 gap-x-5 mb-10">
+                        {newsOffers?.map((newsOffer: NewsOfferInterface) => {
+                            return (
+                                <div key={newsOffer.id} className="md:w-60 sm:w-50 w-40">
+                                    <img
+                                        className="rounded transition ease-in-out delay-150 hover:scale-105 duration-300 cursor-pointer"
+                                        src={newsOffer.img}
+                                        alt={newsOffer.name}
+                                        onError={({ currentTarget }) => {
+                                            currentTarget.onerror = null;
+                                            currentTarget.src = errImage;
+                                        }}
+                                        onClick={() => {
+                                            scroll(0, 0);
+                                            navigate(`/newsoffer/detail/${newsOffer.id}`);
+                                        }}
+                                    />
+                                    <h1
+                                        className="sm:h-12 h-10 line-clamp-2 sm:text-sm text-xs text-center font-semibold uppercase cursor-pointer pt-2 hover:text-sky-500"
+                                        onClick={() => {
+                                            scroll(0, 0);
+                                            navigate(`/newsoffer/detail/${newsOffer.id}`);
+                                        }}
+                                    >
+                                        {newsOffer.name}
+                                    </h1>
+                                    <div className="flex sm:space-x-2 space-x-1 items-center pt-1 sm:px-2 pb-2">
+                                        <CalendarDaysIcon className="sm:h-6 sm:w-6 h-4 w-4" />
+                                        <p className="sm:text-sm text-xs">{newsOffer.date}</p>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
---
+++ src/Pages/NewsOffer/NewsOfferDetails.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from "react";
+import { Layout } from "../../components/Layout";
+import { useGet } from "../../api/get";
+import { NewsOffer as NewsOfferInterface } from "../../interface/Interface";
+import { useParams } from "react-router-dom";
+import errImage from "../../assets/img/news_detail_img_d1.jpg";
+import { NewsOffer } from "../../components/NewsOffer";
+import { Spin } from "antd";
+
+export const NewsOfferDetails = () => {
+    const {
+        fetchGet,
+        result: newsOffer,
+        isLoading,
+    } = useGet<NewsOfferInterface>();
+    const param = useParams();
+    const id = param.id;
+
+    useEffect(() => {
+        fetchGet("newsoffer/detail/" + id);
+    }, [id]);
+
+    return (
+        <>
+            <div className="lg:mx-32 md:mx-10 mx-5 lg:mb-10 md:mb-5 mb-2">
+                {isLoading ? (
+                    <div className="flex justify-center min-h-screen py-10">
+                        <Spin size="large" tip="Loading..." />
+                    </div>
+                ) : (
+                    <>
+                        {newsOffer && (
+                            <div>
+                                <div className="py-6">
+                                    <h2 className="lg:text-2xl md:text-xl text-base font-bold">
+                                        {newsOffer.name}
+                                    </h2>
+                                </div>
+                                <div className="flex sm:flex-row flex-col sm:space-x-8">
+                                    <img
+                                        className="sm:w-72 w-60 h-fit rounded mx-auto"
+                                        src={newsOffer.img}
+                                        alt={newsOffer.name}
+                                        onError={({ currentTarget }) => {
+                                            currentTarget.onerror = null;
+                                            currentTarget.src = errImage;
+                                        }}
+                                    />
+                                    <div className="sm:w-3/5 sm:mt-0 mt-5 w-full space-y-2 sm:text-base">
+                                        <p>
+                                            <b>1. Thời gian áp dụng: </b>
+                                            {newsOffer.date}
+                                        </p>
+                                        <div>
+                                            <b>
+                                                2. Nội dung chương trình: <br />
+                                            </b>
+                                            <ul>
+                                                {newsOffer.contents?.map((content) => (
+                                                    <li>{content}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                        <p>
+                                            <b>3. Địa điểm áp dụng: </b>
+                                            {newsOffer.address}
+                                        </p>
+                                        <p>
+                                            <b>
+                                                4. Đối tượng khuyến mại: <br />
+                                            </b>
+                                            <ul>
+                                                {newsOffer.objects?.map((obj) => (
+                                                    <li>{obj}</li>
+                                                ))}
+                                            </ul>
+                                        </p>
+                                        <div>
+                                            <b>
+                                                5. Điều kiện và điều khoản: <br />
+                                            </b>
+                                            <ul>
+                                                {newsOffer.others?.map((other) => (
+                                                    <li>{other}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
+            <NewsOffer />
+        </>
+    );
+};
