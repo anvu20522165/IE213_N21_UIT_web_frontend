217d2cd09c90128fa23cf166aaf0845a2d39ab1a
PHUOC AN VU NGUYEN <115616152+anvu20522165@users.noreply.github.com>
2023-05-29 16:23:13 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+// galaxy: `url("./src/assets/background_galaxy.jpg")`,
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: `url("https://www.bhdstar.vn/wp-content/themes/bhd/assets/images/movie-details-bg.jpg")`,
+      },
+      fontFamily: {
+        source: ["Source Sans Pro", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      animation: {
+        scale: "scale 0.5s ease-in-out",
+        scaleout: "scaleout 0.5s ease-out",
+      },
+      keyframes: {
+        scale: {
+          "0%": { scale: "0" },
+          "100%": { scale: "1" },
+        },
+        scaleout: {
+          "0%": { scale: "1" },
+          "100%": { scale: "0" },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
