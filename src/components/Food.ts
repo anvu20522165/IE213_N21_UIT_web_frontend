85608f34e1d7920594f10d5a8270c694432d4c50
Vippro67 <tuantu120502@gmail.com>
2023-05-31 00:19:54 +0700

    add some components

---
+++ src/components/Food.ts
@@ -0,0 +1,96 @@
+import { Food } from "../interface/Interface";
+
+export const foodItems: Food[] = [
+  {
+    id: 1,
+    image:
+      "https://www.cgv.vn/media/concession/web/62fe077bd978b_1660815228.png",
+    title: "SINGLE BT21 COMBO",
+    contents: [
+      "1 ly BT21 Back To School 32Oz + 1 nước siêu lớn + 1 bắp 44Oz (tuỳ chọn vị)",
+    ],
+    price: 309000,
+    quantity: 0,
+  },
+  {
+    id: 2,
+    image:
+      "https://www.cgv.vn/media/concession/web/6364866d90d3f_1667532398.png",
+    title: "POWER COMBO",
+    contents: ["1 ly Avatar + 1 nước siêu lơn + 1 bắp ngọt lớn"],
+    price: 200000,
+    quantity: 0,
+  },
+  {
+    id: 3,
+    image:
+      "https://www.cgv.vn/media/concession/web/63648762bc32b_1667532643.png",
+    title: "SUPER POWER COMBO",
+    contents: ["1 ly Avatar + 2 nước siêu lớn + 1 bắp ngọt lớn"],
+    price: 259000,
+    quantity: 0,
+  },
+  {
+    id: 4,
+    image:
+      "https://www.cgv.vn/media/concession/web/62c3ee7b85011_1657007740.png",
+    title: "SNOOPY SINGLE COMBO",
+    contents: ["1 ly Snoopy 32Oz (kèm nước) + 1 bắp ngọt lớn"],
+    price: 239000,
+    quantity: 0,
+  },
+  {
+    id: 5,
+    image:
+      "https://www.cgv.vn/media/concession/web/62c3ef50cb95e_1657007953.png",
+    title: "SNOOPY DOUBLE COMBO",
+    contents: ["2 ly Snoopy 32Oz (kèm nước) + 1 bắp ngọt lớn"],
+    price: 409000,
+    quantity: 0,
+  },
+  {
+    id: 6,
+    image:
+      "https://www.cgv.vn/media/concession/web/62c3efe0cf615_1657008097.png",
+    title: "SNOOPY TRIPLE COMBO",
+    contents: ["3 ly Snoopy 32Oz (kèm nước) + 1 bắp ngọt lớn"],
+    price: 599000,
+    quantity: 0,
+  },
+  {
+    id: 7,
+    image:
+      "https://www.cgv.vn/media/concession/web/62fe09d0e267d_1660815825.png",
+    title: "BT21 BACK TO SCHOOL",
+    contents: ["08 ly BT21 Back To School 32Oz"],
+    price: 2099000,
+    quantity: 0,
+  },
+  {
+    id: 8,
+    image:
+      "https://www.cgv.vn/media/concession/web/625f7a6cbe151_1650424429.png",
+    title: "MY COMBO",
+    contents: ["1 bắp lớn + 1 nước siêu lớn"],
+    price: 83000,
+    quantity: 0,
+  },
+  {
+    id: 9,
+    image:
+      "https://www.cgv.vn/media/concession/web/625f7aace684c_1650424493.png",
+    title: "CGV COMBO",
+    contents: ["1 bắp lớn + 2 nước siêu lớn"],
+    price: 102000,
+    quantity: 0,
+  },
+  {
+    id: 10,
+    image:
+      "https://www.cgv.vn/media/concession/web/625f7ac59a3dd_1650424518.png",
+    title: "CGV SNACK COMBO",
+    contents: ["1 bắp lớn + 2 nước siêu lớn + 1 Snack"],
+    price: 113000,
+    quantity: 0,
+  },
+];
