f84378aa44e323c05bb87244011eca987e607338
Vippro67 <tuantu120502@gmail.com>
2023-06-01 09:21:59 +0700

    add pages buyfood,payment,ticket, add dependencies

--- src/Pages/BookTicket/Booking.tsx
+++ src/Pages/BookTicket/Booking.tsx
@@ -1,47 +1,357 @@
-import React from "react";
+import React, { useState } from "react";
 import { SelectSeats } from "./SelectSeats";
 import { Layout } from "../../components/Layout";
-import { LineWithText } from "../../components/LineWithText";
 import { useGet } from "../../api/get";
-import { Showtime } from "../../interface/Interface";
-import { useParams } from "react-router-dom";
-import ticket_img from "../../assets/ticket_bg.png";
+import { Food, Seat, Showtime } from "../../interface/Interface";
+import { useNavigate, useParams } from "react-router-dom";
+import { BuyFood } from "./BuyFood";
+import { Payment } from "./Payment";
+import { openNotification } from "../../components/Notifications";
+import { usePost } from "../../api/post";
+import { Modal } from "../../components/Modal/Modal";
+import {
+  ClockIcon,
+  CalendarDaysIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/outline";
+import { Steps } from "../../components/Steps";
+
 export const Booking = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user") ?? "null");
   const { fetchGet: fetchShowtime, result: showtimeResult } =
     useGet<Showtime>();
+  const {
+    fetchPost: fetchBooking,
+    result: bookingResult,
+    isError,
+  } = usePost<any>();
+  const [step, setStep] = useState<number>(1);
+  const [listSelectedSeats, setListSelectedSeats] = useState<Seat[]>([]);
+  const [listFoods, setListFoods] = useState<Food[]>([]);
+  const [foodPrice, setFoodPrice] = useState<number>(0);
+  const [seatPrice, setSeatPrice] = useState<number>(0);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const [paymentMethod, setPaymentMethod] = useState<any>();
+
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [ageVerification, setAgeVerification] = useState<boolean>(false);
+
   const param = useParams();
   const id = param.id;
   React.useEffect(() => {
     fetchShowtime("showtime/" + id);
   }, []);
 
-  return (
-    <Layout>
-      <LineWithText>BOOKING ONLINE</LineWithText>
-      <div className="font-bold px-32">
-        {showtimeResult?.cinema.name} | Phòng{" "}
-        {showtimeResult?.showtime.roomId.name} | Số ghế (240/240)
-      </div>
-      <div className="font-bold px-32">
-        {showtimeResult?.showtime.date.substring(0, 10)}{" "}
-        {showtimeResult?.showtime.time} - {showtimeResult?.showtime.time_end}
-      </div>
-      <LineWithText>CHỌN GHẾ</LineWithText>
-      <SelectSeats></SelectSeats>
+  const handlePreviousClick = (step: number) => {
+    if (step > 2) {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      setStep(step - 1);
+    } else if (step === 2) {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      setAgeVerification(false);
+      setStep(step - 1);
+    }
+  };
 
-      <div
-        className="bg-center bg-no-repeat"
-        style={{
-          backgroundImage: `url(${ticket_img})`,
-        }}
-      >
-        <div className="grid grid-cols-3 py-[70px] px-[150px]">
-          <img
-            src={showtimeResult?.showtime.movieId.image}
-            className="w-[200px]"
-          ></img>
+  React.useEffect(() => {
+    if (ageVerification) {
+      setStep(2);
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [ageVerification]);
+
+  const handleNextClick = (step: number) => {
+    if (step === 1) {
+      if (seatPrice === 0) {
+        openNotification("info", "Vui lòng chọn ghế để tiếp tục");
+        return;
+      } else {
+        if (showtimeResult?.showtime.movieId.rated.substring(0, 1) === "P") {
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+          setStep(step + 1);
+        } else setOpenModal(true);
+      }
+    } else {
+      if (step < 3) {
+        window.scroll({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+        setStep(step + 1);
+      } else if (step === 3) {
+        if (paymentMethod) {
+          bookTicket();
+        } else {
+          openNotification("info", "Vui lòng chọn phương thức thanh toán");
+        }
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    if (bookingResult) {
+      if (!isError) {
+        if (bookingResult?.ticket?.Showtime) {
+          openNotification("success", "Thanh toán thành công");
+          scroll(0, 0);
+          navigate("/ticket/" + bookingResult?.ticket?._id);
+        }
+      } else {
+        openNotification(
+          "error",
+          "Vé bạn chọn đã có người đặt, vui lòng đặt lại"
+        );
+      }
+    }
+  }, [bookingResult]);
+
+  React.useEffect(() => {
+    let price = 0;
+    listFoods.map((food: Food) => {
+      if (food.quantity > 0) {
+        price += food.quantity * food.price;
+      }
+    });
+    setFoodPrice(price);
+  }, [listFoods]);
+
+  React.useEffect(() => {
+    let price = 0;
+    listSelectedSeats?.map((seat: Seat) => {
+      price += seat.price;
+    });
+    setSeatPrice(price);
+  }, [listSelectedSeats]);
+
+  React.useEffect(() => {
+    setTotalPrice(foodPrice + seatPrice);
+  }, [foodPrice, seatPrice]);
+
+  React.useEffect(() => {}, [foodPrice, seatPrice]);
+
+  const bookTicket = () => {
+    let code: number[] = [];
+    listSelectedSeats?.map((seat: Seat) => {
+      code.push(seat.id);
+    });
+
+    let listFood: Food[] = [];
+
+    listFoods.map((food: Food) => {
+      if (food.quantity > 0) {
+        listFood.push(food);
+      }
+    });
+
+    fetchBooking(
+      {
+        showtime: id,
+        user: user._id,
+        seat: code,
+        foods: listFood,
+        paymentMethod: paymentMethod.name,
+      },
+      "ticket"
+    );
+  };
+
+  return (
+    <>
+      <Modal
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        setConfirm={setAgeVerification}
+        title="Xác nhận độ tuổi"
+        content={
+          "Tôi xác nhận mua vé cho người xem từ " +
+          showtimeResult?.showtime.movieId.rated.substring(1, 3) +
+          " tuổi trở lên và hiểu rằng UIT CINEMA sẽ không hoàn tiền nếu không chứng thực độ tuổi của khán giả."
+        }
+      ></Modal>
+
+      <div className="py-5">
+        <Steps currentStep={step}></Steps>
+      </div>
+
+      {step === 1 && (
+        <SelectSeats
+          soldSeats={showtimeResult?.showtime.seats}
+          setListSelectedSeats={setListSelectedSeats}
+        ></SelectSeats>
+      )}
+
+      {step === 2 && <BuyFood setListFoods={setListFoods}></BuyFood>}
+
+      {step === 3 && (
+        <Payment
+          seatPrice={seatPrice}
+          listFoods={listFoods}
+          totalPrice={totalPrice}
+          setSelectedPayment={setPaymentMethod}
+        ></Payment>
+      )}
+
+      <div className="md:sticky md:bottom-0 md:py-0 py-10 flex justify-center">
+        <div className="mx-auto dark:text-black">
+          {/* <div className="md:fixed md:bottom-0 md:left-1/2 md:transform md:-translate-x-1/2 md:py-0 py-10"> */}
+          <div className="relative">
+            <div className="z-10 absolute md:top-1/2 bottom-[-10px] md:left-0 left-10 transform -translate-y-1/2">
+              <button
+                className="border-2 dark:border-slate-800 bg-gray-700 w-[75px] h-[75px] rounded-lg"
+                onClick={() => handlePreviousClick(step)}
+              >
+                <i className="fa-sharp fa-solid fa-arrow-left text-[24px] mt-[13px] mx-[25px] text-white text-center"></i>
+                <div className="text-[12px] text-white text-center">
+                  PREVIOUS
+                </div>
+              </button>
+            </div>
+
+            <div className="z-10 absolute md:top-1/2 bottom-[-10px] md:right-0 right-10 transform -translate-y-1/2">
+              <button
+                className="border-2 dark:border-slate-800 bg-sky-600 w-[75px] h-[75px] rounded-lg"
+                onClick={() => handleNextClick(step)}
+              >
+                {step === 3 ? (
+                  <>
+                    <i className="fa-solid fa-money-check-dollar text-[24px] mt-[13px] mx-[22px] text-white text-center"></i>
+                    <div className="text-[12px] text-white text-center">
+                      PAYMENT
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <i className="fa-sharp fa-solid fa-arrow-right text-[24px] mt-[13px] mx-[25px] text-white text-center"></i>
+                    <div className="text-[12px] text-white text-center">
+                      NEXT
+                    </div>
+                  </>
+                )}
+              </button>
+            </div>
+
+            <div className="relative md:h-auto h-[360px] flex md:flex-row md:space-x-5 space-x-0 flex-col md:space-y-0 space-y-5  lg:px-10 px-10 md:mx-20 mx-2 md:py-2 pt-5 bg-gradient-to-r from-sky-300 to-indigo-300 rounded">
+              <div className="flex justify-center items-center space-x-5">
+                <img
+                  src={showtimeResult?.showtime.movieId.image}
+                  className="w-[90px] h-[120px] rounded"
+                ></img>
+                <div className="flex flex-col justify-between space-y-1 lg:w-[300px] md:w-[200px] ">
+                  <p className="font-bold line-clamp-2">
+                    {showtimeResult?.showtime.movieId.rated.substring(0, 1) ===
+                    "P" ? (
+                      <span className="border border-green-600 rounded text-green-600 px-1 mr-1">
+                        P
+                      </span>
+                    ) : (
+                      <span className="border border-red-500 rounded text-red-500 px-1 mr-1">
+                        {showtimeResult?.showtime.movieId.rated.substring(0, 3)}
+                      </span>
+                    )}
+                    {showtimeResult?.showtime.movieId.name}
+                  </p>
+
+                  <div className="flex items-center">
+                    <p className="font-semibold">
+                      <MapPinIcon className="mr-2 h-5 w-5 inline-block" />
+                      {showtimeResult?.cinema.name}
+                    </p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <ClockIcon className="mr-2 h-5 w-5 inline-block" />
+                    <p className="font-semibold">
+                      {showtimeResult?.showtime.time} -{" "}
+                      {showtimeResult?.showtime.time_end}
+                    </p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <CalendarDaysIcon className="mr-2 h-5 w-5 inline-block" />
+                    {showtimeResult && (
+                      <p className="font-semibold">
+                        {new Date(
+                          showtimeResult?.showtime.date
+                        ).toLocaleDateString("en-UK")}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div className="md:flex grid grid-cols-2">
+                <div className="w-[150px] grid md:grid-rows-3 items-start md:pt-2">
+                  <p>
+                    Phòng:{" "}
+                    <span className="font-bold">
+                      {showtimeResult?.showtime.roomId.name}
+                    </span>
+                  </p>
+
+                  <p className="row-span-2">
+                    Ghế:{" "}
+                    <span className="font-bold">
+                      {listSelectedSeats?.map((seat: Seat) => {
+                        if (
+                          listSelectedSeats[listSelectedSeats.length - 1].id ==
+                          seat.id
+                        )
+                          return <span key={seat.code}>{seat.code}</span>;
+                        else return <span key={seat.code}>{seat.code}, </span>;
+                      })}
+                    </span>
+                  </p>
+                </div>
+                <div className="grid grid-cols-2 items-center md:w-[200px] w-[160px]">
+                  <div>Vé phim:</div>
+                  <div className="font-bold">
+                    {" "}
+                    {seatPrice.toLocaleString("vi", {
+                      style: "currency",
+                      currency: "VND",
+                    })}
+                  </div>
+                  <div>Combo:</div>
+                  <div className="font-bold">
+                    {foodPrice.toLocaleString("vi", {
+                      style: "currency",
+                      currency: "VND",
+                    })}
+                  </div>
+                  <div>Tổng:</div>
+                  <div className="font-bold text-xl">
+                    {" "}
+                    {totalPrice.toLocaleString("vi", {
+                      style: "currency",
+                      currency: "VND",
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </Layout>
+      <div className="h-[40px]"></div>
+    </>
   );
 };
---
+++ src/Pages/BookTicket/Payment.tsx
@@ -0,0 +1,273 @@
+import React, { useEffect, useState } from "react";
+import { TagsOutlined } from "@ant-design/icons";
+import { Food } from "../../interface/Interface";
+import { RadioGroup } from "@headlessui/react";
+import {
+  faCcMastercard,
+  faCcVisa,
+  faCcPaypal,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const payments = [
+  {
+    name: "Thẻ Mastercard",
+    icon: faCcMastercard,
+  },
+  {
+    name: "Thẻ Visa",
+    icon: faCcVisa,
+  },
+  {
+    name: "Thẻ Paypal",
+    icon: faCcPaypal,
+  },
+];
+
+export const Payment: React.FC<{
+  seatPrice: number;
+  listFoods: Food[];
+  totalPrice: number;
+  setSelectedPayment: React.Dispatch<React.SetStateAction<string | undefined>>;
+}> = ({ seatPrice, listFoods, totalPrice, setSelectedPayment }) => {
+  const [typeVoucher, setTypeVoucher] = React.useState<0 | 1 | 2>(0);
+
+  const [timeLeft, setTimeLeft] = useState(5 * 60);
+
+  useEffect(() => {
+    if (timeLeft === 0) return;
+
+    const countdownInterval = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
+  }, [timeLeft]);
+
+  const minutes = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (timeLeft % 60).toString().padStart(2, "0");
+
+  return (
+    <div>
+      <div className="grid sm:grid-cols-2 sm:mx-20 mx-5 sm:space-x-10 ">
+        <div className="space-y-4">
+          <div className="rounded h-auto py-4 px-7 space-y-3 bg-white dark:bg-slate-700 drop-shadow-md">
+            <TagsOutlined className="text-[24px]" />
+            <span className="font-bold text-[16px] mx-2">GIẢM GIÁ</span>
+            <button
+              className="block w-full text-left rounded text-[16px] bg-sky-200 dark:bg-sky-800 p-2"
+              onClick={() => {
+                typeVoucher === 1 ? setTypeVoucher(0) : setTypeVoucher(1);
+              }}
+            >
+              Điểm
+            </button>
+
+            {typeVoucher == 1 && (
+              <div className="py-2 space-y-3">
+                <div className="flex justify-between mx-4">
+                  <p>Điểm của bạn</p>
+                  <p>999 điểm</p>
+                </div>
+
+                <div className="flex justify-between mx-4">
+                  <p>Nhập số điểm</p>
+                  <input
+                    type="number"
+                    min={0}
+                    max={999}
+                    className="w-44 p-2 rounded dark:bg-slate-800 border border-white"
+                  />
+                </div>
+
+                <div className="flex justify-end px-4">
+                  <button className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+                    Xác nhận
+                  </button>
+                </div>
+              </div>
+            )}
+
+            <button
+              className="block w-full text-left rounded text-[16px] bg-sky-200 dark:bg-sky-800 p-2"
+              onClick={() => {
+                typeVoucher === 2 ? setTypeVoucher(0) : setTypeVoucher(2);
+              }}
+            >
+              Voucher
+            </button>
+
+            {typeVoucher === 2 && (
+              <div className="py-2 space-y-3">
+                <div className="flex justify-between mx-4">
+                  <p>Nhập mã voucher</p>
+                  <input className="w-44 p-2 rounded dark:bg-slate-800 border border-white"></input>
+                </div>
+
+                <div className="flex justify-end px-4">
+                  <button className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+                    Xác nhận
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="rounded h-auto p-4 space-y-3 bg-white dark:bg-slate-700 drop-shadow-md">
+            <i className="fa-solid fa-money-check-dollar text-[24px]"></i>
+
+            <div className="inline-flex font-bold text-[16px] mx-2">
+              PHƯƠNG THỨC THANH TOÁN
+            </div>
+
+            <div className="w-full px-2 py-2">
+              <div className="mx-auto w-full max-w-md">
+                <RadioGroup onChange={setSelectedPayment}>
+                  <div className="space-y-3">
+                    {payments.map((payment) => (
+                      <RadioGroup.Option
+                        key={payment.name}
+                        value={payment}
+                        className={({ active, checked }) =>
+                          `${
+                            active
+                              ? "ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300"
+                              : ""
+                          }
+                          ${
+                            checked
+                              ? "bg-sky-700 bg-opacity-75 text-white"
+                              : "bg-white dark:bg-slate-700"
+                          }
+                          drop-shadow-md border relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                        }
+                      >
+                        {({ active, checked }) => (
+                          <>
+                            <div className="flex w-full items-center justify-between">
+                              <div className="flex items-center">
+                                <div className="text-sm">
+                                  <RadioGroup.Label
+                                    as="p"
+                                    className={`font-medium flex items-center justify-between ${
+                                      checked
+                                        ? "text-white"
+                                        : "text-gray-900 dark:text-white"
+                                    }`}
+                                  >
+                                    <FontAwesomeIcon
+                                      icon={payment.icon}
+                                      className="mr-3 text-[24px]"
+                                    />
+                                    {payment.name}
+                                  </RadioGroup.Label>
+                                </div>
+                              </div>
+                              {checked && (
+                                <div className="shrink-0 text-white">
+                                  <CheckIcon className="h-6 w-6" />
+                                </div>
+                              )}
+                            </div>
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-4 sm:mt-0 mt-4">
+          <div className="bg-white dark:bg-slate-700 drop-shadow-md rounded h-auto p-4 space-y-3">
+            <p className="text-center font-bold text-[16px]">
+              THÔNG TIN ĐƠN HÀNG
+            </p>
+            <div className="flex justify-between">
+              <p>Giá vé</p>
+              <p>
+                {seatPrice.toLocaleString("vi", {
+                  style: "currency",
+                  currency: "VND",
+                })}
+              </p>
+            </div>
+
+            {listFoods.map((food: Food) => (
+              <div key={food.id}>
+                {food.quantity > 0 && (
+                  <div>
+                    <div className="flex justify-between">
+                      <p>
+                        {food.title} x {food.quantity}
+                      </p>
+                      <p>
+                        {(food.price * food.quantity).toLocaleString("vi", {
+                          style: "currency",
+                          currency: "VND",
+                        })}
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+
+            <div className="flex justify-between font-bold text-[16px]">
+              <p>Tổng</p>
+              <p>
+                {totalPrice.toLocaleString("vi", {
+                  style: "currency",
+                  currency: "VND",
+                })}
+              </p>
+            </div>
+
+            <div className="flex justify-between font-bold text-[16px]">
+              <p>Khuyến mãi</p>
+              <p>0 ₫</p>
+            </div>
+
+            <div className="flex justify-between font-bold text-[16px]">
+              <p>Thanh toán</p>
+              <p>
+                {totalPrice.toLocaleString("vi", {
+                  style: "currency",
+                  currency: "VND",
+                })}
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white dark:bg-slate-700 drop-shadow-md rounded h-auto p-4 space-y-3">
+            <div className="flex items-center justify-center">
+              <div className="flex flex-col items-center justify-center">
+                <div className="text-xl font-bold">Thời hạn thanh toán</div>
+                <div className="text-5xl font-bold my-2">{`${minutes}:${seconds}`}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function CheckIcon(props: any) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
